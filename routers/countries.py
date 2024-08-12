904c269d8cbfd03787f67effc4bd62e6df445cc2
Piotr Dr <sudoku589@gmail.com>
2024-08-12 20:35:57 +0200

    csv and json format as response

--- routers/countries.py
+++ routers/countries.py
@@ -1,5 +1,4 @@
 from services import countries_service
-
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -11,21 +10,25 @@ async def get_country(country: str):
 
 
 @router.get("/region/{region}", tags=["countries"])
-async def get_region_biggest_countries(region: str):
+async def get_region_biggest_countries(region: str, response_format: str):
     """
     Gets a json containing Country Name, Capital, Region, Sub Region, Population, Area, Borders
     :param region:
+    :param response_format:
     :return: json
     """
-    return countries_service.get_region_biggest_countries(region=region)
+    return countries_service.get_region_biggest_countries(region=region,
+                                                          response_format=response_format)
 
 
 @router.get("/subregion/{subregion}", tags=["countries"])
-async def get_subregion_countries_with_neighbours(subregion: str):
+async def get_subregion_countries_with_neighbours(subregion: str, response_format: str):
     """
     Gets a json containing Country Name, Capital, Region, Sub Region, Population, Area, Borders of country with
     neighbours
     :param subregion:
+    :param response_format:
     :return: json
     """
-    return countries_service.get_subregion_countries_with_neighbours(subregion=subregion)
+    return countries_service.get_subregion_countries_with_neighbours(subregion=subregion,
+                                                                     response_format=response_format)
--- utils/countries.py
+++ utils/countries.py
@@ -1,9 +1,15 @@
 import json
+import os
+import tempfile
+
 import pandas as pd
+from fastapi import HTTPException
+from fastapi.responses import FileResponse
+from starlette.background import BackgroundTask
 
 
 def get_biggest_countries_in_region(region: list, how_many_countries: int) -> list:
-    sorted_countries = sorted(region, key=lambda d: d['area'])[-how_many_countries:-1]
+    sorted_countries = sorted(region, key=lambda d: d['area'])[-how_many_countries-1:-1]
     return sorted_countries
 
 
@@ -12,13 +18,23 @@ def get_countries_with_neighbours(subregion: list, at_least_neighbours: int):
     for country in subregion:
         if len(country['borders']) >= at_least_neighbours:
             countries_with_neighbours.append(country)
+    if not countries_with_neighbours:
+        raise HTTPException(status_code=404, detail=f"No countries with more than {at_least_neighbours}"
+                                                    f" borders found in this subregion")
     return countries_with_neighbours
 
 
-def convert_json_to_csv(file_json):
-    with open(file_json) as json_file:
-        json_data = json.load(json_file)
-
-    csv_data = pd.DataFrame(json_data).to_csv('out.csv', index=False)
-
-    return csv_data
+def return_data_file(json_response, temp_format):
+    if temp_format == "json":
+        fd, path = tempfile.mkstemp(suffix=f'.{temp_format}')
+        json_object = json.dumps(json_response)
+        with os.fdopen(fd, mode='w') as f:
+            f.write(json_object)
+        return FileResponse(path, media_type="application/json", background=BackgroundTask(os.remove, path))
+    elif temp_format == "csv":
+        with tempfile.NamedTemporaryFile(delete=False) as temp:
+            pd.DataFrame(json_response).to_csv(path_or_buf=temp.name + '.csv')
+            temp.close()
+        return FileResponse(temp.name + '.csv',
+                            media_type="text/csv",
+                            background=BackgroundTask(os.remove, temp.name + '.csv'))
